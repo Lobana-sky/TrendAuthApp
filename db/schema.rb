4da6126a53d90bbffb0bd09082079110ce387f37
loubana alarabi <loubana@dconfig.com>
2021-08-08 15:31:39 +0300

    Account gets locked after 10 retries and user can unlock

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,9 @@ ActiveRecord::Schema.define(version: 2021_08_08_084127) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
