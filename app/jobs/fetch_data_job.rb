63bd5a320ed037d04ec582af53672aede3c0c064
loubana alarabi <loubana@dconfig.com>
2021-08-23 23:26:50 +0300

    clean code

--- app/jobs/fetch_data_job.rb
+++ app/jobs/fetch_data_job.rb
@@ -6,18 +6,18 @@ class FetchDataJob < ApplicationJob
   end
 
   def prepare_news_table
-      latest_news = fetch_current_news()
-      create_news_and_save(latest_news)
+    latest_news = fetch_current_news()
+    create_news_and_save(latest_news)
   end
   
   def get_category_id(news_category)
     category_name = news_category.empty? ? "None" : news_category[0]
     category = Category.find_by(category_name: category_name)
-      if category != nil
-        return category.id 
-      else 
-        return Category.create(category_name: category_name).id
-      end
+    if category != nil
+      return category.id 
+    else 
+      return Category.create(category_name: category_name).id
+    end
   end
 
   def fetch_current_news
@@ -29,15 +29,11 @@ class FetchDataJob < ApplicationJob
     latest_news.each do |news|
       if CurrentNews.find_by(id_news: news['id']) == nil 
         category_id = get_category_id(news['category'])
-        @current = CurrentNews.create(id_news: news['id'], title: news['title'], description: news['description'],
+        new_current_news = CurrentNews.create(id_news: news['id'], title: news['title'], description: news['description'],
           url: news['url'], author: news['author'], image: news['image'], language: news['language'],
           published: news['published'], category_id: category_id)
-        @current.save
+        new_current_news.save
       end
     end 
   end 
 end
-
-CurrentNews.create(id_news: "f", title: 'title', description: 'description',
-  url: 'url', author: 'author', image: 'image', language: 'language',
-  published: 'published', category_id: 160)
