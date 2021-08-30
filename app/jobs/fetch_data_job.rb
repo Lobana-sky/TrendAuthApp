86559380cd68d503d2c673f46f9e71ff7aef43ab
loubana alarabi <loubana@dconfig.com>
2021-08-30 16:08:02 +0300

    make persist_to_data_base private

--- app/jobs/fetch_data_job.rb
+++ app/jobs/fetch_data_job.rb
@@ -1,17 +1,19 @@
 class FetchDataJob < ApplicationJob
   queue_as :default
 
-  def persist_to_data_base(mapped_latest_news)
-    mapped_latest_news.each do |new_news|
-    CreateCurrentNews.new(current_news_params: new_news).call()
-    end
-  end
-
   def perform(*args)
     results = CurrentNewsApiService.new({endpoint: 'latest-news'}).call()
     if results[:payload] && results[:success]
       mapped_latest_news = ResponseMapping.new(results[:payload]).call()
       persist_to_data_base(mapped_latest_news)
     end
+  end
+
+  private
+
+    def persist_to_data_base(mapped_latest_news)
+      mapped_latest_news.each do |new_news|
+      CreateCurrentNews.new(current_news_params: new_news).call()
+      end
   end
 end
