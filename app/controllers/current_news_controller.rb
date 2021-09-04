083d8077ae286fd5a0577fdf9c3b3f304a715ade
loubana alarabi <loubana@dconfig.com>
2021-09-04 21:45:47 +0300

    add category service

--- app/controllers/current_news_controller.rb
+++ app/controllers/current_news_controller.rb
@@ -1,6 +1,7 @@
 class CurrentNewsController < ApplicationController
   def index 
     @current_news = CurrentNews.with_published_ASC.page params[:page]
-    @categories = Category.find_each
+    categories = Category.find_each
+    @categories = GetCategoriesAsHash.new(categories).call()
   end
 end
