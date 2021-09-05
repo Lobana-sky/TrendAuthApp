09cc8745c7101c47bcdcc84127b7416f67d6b563
loubana alarabi <loubana@dconfig.com>
2021-09-06 00:23:40 +0300

    add get category as hash to model

--- app/controllers/current_news_controller.rb
+++ app/controllers/current_news_controller.rb
@@ -2,6 +2,6 @@ class CurrentNewsController < ApplicationController
   def index 
     @current_news = CurrentNews.with_published_ASC.page params[:page]
     categories = Category.find_each
-    @categories = GetCategoriesAsHash.new(categories).call()
+    @categories = Category.get_categories_as_hash(categories)
   end
 end
