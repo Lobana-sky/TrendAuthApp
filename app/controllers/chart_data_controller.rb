024aed01e6e30b938a6fe387e78f6d1105bf2fa9
loubana alarabi <loubana@dconfig.com>
2021-09-06 17:41:01 +0300

    refactoring

--- app/controllers/chart_data_controller.rb
+++ app/controllers/chart_data_controller.rb
@@ -1,25 +1,14 @@
 class ChartDataController < ApplicationController
   def index
-    @search_results_seven_days = get_chart_data()
-    # @category_name_search = Category.find(params[:search_word]["id"].to_i).category_name
-    @category_id_search = Category.find(params[:search_word]["id"].to_i)
-    @all_category_count = CurrentNews.where(category_id: @category_id_search).group_by_day(:published).count
-    @all_current_news_by_published_date = CurrentNews.group_by_day(:published).count
-  end
+    id_for_searching_category = params[:search_word]["id"].to_i
+    @name_for_searching_category = Category.find(id_for_searching_category).category_name
 
-  def get_chart_data
-    chart_date = [];
-    time = Time.new
-    time = time.prev_day(day = 6)
-    for i in 1..7
-      start_time = time.beginning_of_day().advance(hours: 3)
-      end_time = time.end_of_day().advance(hours: 3)
-      search_results = CurrentNews.where(category_id: params[:search_word]["id"].to_i)
-      .where(published: start_time...end_time).count
-        # add to the beginning of array
-      chart_date.push([time.strftime('%A'), search_results])
-      time = time.next_day(day = 1)
-    end
-  return chart_date
+    @get_data_for_category_prev_week = 
+      CurrentNews.get_data_for_category_prev_week(id_for_searching_category)
+    @get_data_for_category_entire_time = 
+      CurrentNews.get_data_for_category_entire_time(id_for_searching_category)
+    @all_current_news_by_published_date = 
+      CurrentNews.get_current_news_by_published()
   end
+    
 end
