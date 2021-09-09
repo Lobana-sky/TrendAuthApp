e9bdb09890f5d5a126a2bba216d0525ef5437603
loubana alarabi <loubana@dconfig.com>
2021-09-09 18:17:10 +0300

    add filters

--- app/controllers/chart_data_controller.rb
+++ app/controllers/chart_data_controller.rb
@@ -1,14 +1,35 @@
 class ChartDataController < ApplicationController
   def index
-    id_for_searching_category = params[:search_word]["id"].to_i
-    @name_for_searching_category = Category.find(id_for_searching_category).category_name
+    # Full chart all current news group by published date
+    @current_news_by_published = 
+      CurrentNews.get_current_news_group_by_published()
 
-    @get_data_for_category_prev_week = 
-      CurrentNews.get_data_for_category_prev_week(id_for_searching_category)
-    @get_data_for_category_entire_time = 
-      CurrentNews.get_data_for_category_entire_time(id_for_searching_category)
-    @all_current_news_by_published_date = 
-      CurrentNews.get_current_news_by_published()
+    # Trend chart seacrh by user (search word and no of days)
+    if params[:query].present?
+      @trend_data = 
+        CurrentNews.get_trend_data(params[:query], params[:days_no].to_i)
+      @search_word = params[:query]#pass in to view
+    end
+
+    # Category chart : search by category
+    if params[:search_word].present?
+      category_id = get_category_id()
+      @category_name = get_category_name(category_id)#pass in to view
+
+      @current_news_by_category_in_entire_time = 
+      CurrentNews.get_current_news_by_category(category_id)
+    end
+
   end
+
+  private
+
+    def get_category_id()
+      params[:search_word]["id"].to_i
+    end
+
+    def get_category_name(id)
+      Category.find(id).category_name
+    end
     
 end
--- app/models/current_news.rb
+++ app/models/current_news.rb
@@ -8,31 +8,34 @@ class CurrentNews < ApplicationRecord
   # we can set a specific value for a specific module
   paginates_per 25
   scope :with_published_ASC, -> { order('published ASC') }
-
-  def self.get_current_news_by_published
+  
+  
+  
+  # Charts Data
+  def self.get_current_news_group_by_published
     CurrentNews.group_by_day(:published).count
   end
 
-  def self.get_data_for_category_entire_time(id)
-    CurrentNews.where(category_id: id).group_by_day(:published).count
-  end
-
-  def self.get_data_for_category_prev_week(id)
+  def self.get_trend_data(search_word, no_of_days)
     chart_date = [];
     time = Time.new
-    #cuz we need 7 days before today, we will got the start day by going in past 6 days
-    time = time.prev_day(day = 6) #and then we will go to the next day every loop step
-    for i in 1..7
+    time = time.prev_day(day = no_of_days - 1)
+    for i in 1..no_of_days
       start_time = time.beginning_of_day().advance(hours: 3)
       end_time = time.end_of_day().advance(hours: 3)
-      search_results = CurrentNews.where(category_id: id)
+      search_results = CurrentNews.where('title like ?', "%#{search_word}%")
       .where(published: start_time...end_time).count
+
       # add to the beginning of array
       #We push the name of the day 'time.strftime('%A')' and category count in that day
-      chart_date.push([time.strftime('%A'), search_results])
-      #and then we will go to the next day every loop step
+      chart_date.push([time.strftime("%A"), search_results])
       time = time.next_day(day = 1)
     end
     return chart_date
   end
+
+  def self.get_current_news_by_category(id)
+    CurrentNews.where(category_id: id).group_by_day(:published).count
+  end
+
 end
