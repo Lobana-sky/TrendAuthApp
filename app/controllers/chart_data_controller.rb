512cf56d67ef5ea2c1a55109c1603089a03195b7
loubana alarabi <loubana@dconfig.com>
2021-09-12 22:50:25 +0300

    chart filter

--- app/controllers/chart_data_controller.rb
+++ app/controllers/chart_data_controller.rb
@@ -2,24 +2,31 @@ class ChartDataController < ApplicationController
   def index
     @current_news = CurrentNews.find_each
     @categories = Category.find_each
+
     # Full chart all current news group by published date
     @current_news_by_published = 
-      CurrentNews.get_current_news_group_by_published()
+      ChartData.get_current_news_group_by_published()
 
     # Trend chart seacrh by user (search word and no of days)
-    if params[:query].present?
+    if params[:trend_text].present?
       @trend_data = 
-        CurrentNews.get_trend_data(params[:query], params[:days_no].to_i)
-      @search_word = params[:query]#pass in to view
+      ChartData.get_trend_data(params[:trend_text], params[:days_no].to_i)
+      @search_word = params[:trend_text]#pass in to view
+      @show_trend_chart = true
+    else
+      @show_trend_chart = false
     end
 
     # Category chart : search by category
-    if params[:search_word].present?
+    if params[:category].present? && get_category_name(get_category_id()) != "NONE"
       category_id = get_category_id()
       @category_name = get_category_name(category_id)#pass in to view
 
-      @current_news_by_category_in_entire_time = 
-      CurrentNews.get_current_news_by_category(category_id)
+      @current_news_by_category = 
+      ChartData.get_current_news_by_category(category_id)
+      @show_category_chart = true
+    else
+      @show_category_chart = false
     end
 
   end
@@ -27,7 +34,7 @@ class ChartDataController < ApplicationController
   private
 
     def get_category_id()
-      params[:search_word]["id"].to_i
+      params[:category]["id"].to_i
     end
 
     def get_category_name(id)
--- app/controllers/current_news_controller.rb
+++ app/controllers/current_news_controller.rb
@@ -3,5 +3,16 @@ class CurrentNewsController < ApplicationController
     @current_news = CurrentNews.with_published_ASC.page params[:page]
     @categories = Category.find_each
     @categories_hash = Category.get_categories_as_hash(@categories)
+
   end
+
+  # private
+
+  #   def get_category_id()
+  #     params[:category]["id"].to_i
+  #   end
+
+  #   def get_category_name(id)
+  #     Category.find(id).category_name
+  #   end
 end
