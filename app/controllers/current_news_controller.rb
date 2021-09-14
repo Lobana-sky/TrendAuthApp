b0884c4e4ac68113a915885086ba3107720182ff
loubana alarabi <loubana@dconfig.com>
2021-09-14 19:52:14 +0300

    rm categories hash

--- app/controllers/current_news_controller.rb
+++ app/controllers/current_news_controller.rb
@@ -2,6 +2,5 @@ class CurrentNewsController < ApplicationController
   def index 
     @current_news = CurrentNews.with_published_ASC.page params[:page]
     categories = Category.find_each
-    @categories = Category.get_categories_as_hash(categories)
   end
 end
