a2103d8b611a3e7bc3fef410b177d4c64bc5cf6e
loubana alarabi <loubana@dconfig.com>
2021-10-02 10:15:38 +0300

    reply comments

--- app/controllers/current_news_controller.rb
+++ app/controllers/current_news_controller.rb
@@ -1,6 +1,11 @@
 class CurrentNewsController < ApplicationController
   def index 
-    @current_news = CurrentNews.with_published_ASC.page params[:page]
-    categories = Category.find_each
+    @categories = Category.find_each
+
+    @current_news = get_current_news()
+
+    # to pass in for view stuff for the user to know which date and which category he/she select
+    @selected_published = fill_in_published()
+    @selected_category = fill_in_category_name()
   end
 end
