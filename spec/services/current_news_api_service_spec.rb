7b7d60f6182e1d6908d49989808fdf03235c556a
loubana alarabi <loubana@dconfig.com>
2021-10-11 17:08:19 +0300

    clean code

--- spec/services/current_news_api_service_spec.rb
+++ spec/services/current_news_api_service_spec.rb
@@ -1,13 +1,9 @@
 require 'rails_helper'
 require 'current_news_api_service'
-# require 'rest_client'
 
 RSpec.describe CurrentNewsApiService, :type => :request do
   
   it 'should return Current News data' do 
-    # result = RestClient.get 'https://api.currentsapi.services/v1/latest-news?apiKey=ppvVKctrJQz1oZb-RzjjrHr4fm34daUJqV-X3ZITDNbumJgL', 
-    # :content_type => :json, :accept => :json
-    # expect(result.code).to eq(200)
     news = CurrentNewsApiService.new({endpoint: 'latest-news'}).call()
     expect(news).not_to be_empty
   end
