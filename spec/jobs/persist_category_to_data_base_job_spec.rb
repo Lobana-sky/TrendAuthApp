a5ac2a96bd38bcdbe5429cb6cb64f8008bb714a1
loubana alarabi <loubana@dconfig.com>
2021-10-14 20:24:52 +0300

    fix let

--- spec/jobs/persist_category_to_data_base_job_spec.rb
+++ spec/jobs/persist_category_to_data_base_job_spec.rb
@@ -4,16 +4,14 @@ RSpec.describe PersistCategoryToDataBaseJob, type: :job do
   before do
     ActiveJob::Base.queue_adapter = :test
   end
-  
+  let(:category_name) { 'CATEGORY' }
   it "PersistCategoryToDataBaseJob enqueued" do
-    category_name = 'CATEGORY'
     expect {
       PersistCategoryToDataBaseJob.set(queue: "default").perform_later(category_name)
     }.to have_enqueued_job.with(category_name).on_queue("default").at(:no_wait)
   end
 # Checking perform
   describe "#perform", :vcr do
-    category_name = 'CATEGORY'
     it "add category to category table" do
       expect { PersistCategoryToDataBaseJob.perform_now(category_name) 
       }.to change(Category, :count).by(1)
