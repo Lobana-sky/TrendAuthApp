db5d190285833788c0134d6f4914f77c7b3b18e6
loubana alarabi <loubana@dconfig.com>
2021-10-28 18:34:31 +0300

    4001 to 3000

--- app/controllers/current_news_controller.rb
+++ app/controllers/current_news_controller.rb
@@ -2,9 +2,17 @@ class CurrentNewsController < ApplicationController
   def index 
     @categories = Category.find_each
 
+
+    # @categories_json = []
+
+    # categories.each do |category|
+    #   @categories_json << category.to_json
+    # end
+
+
     @current_news = get_current_news()
 
-    # to pass in for view stuff for the user to know which date and which category he/she select
+    to pass in for view stuff for the user to know which date and which category he/she select
     @selected_published = fill_in_published()
     @selected_category = fill_in_category_name()
   end
