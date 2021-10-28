e9182966f17be49fa324f5d80a32cc0edbde21b1
loubana alarabi <loubana@dconfig.com>
2021-10-28 18:49:26 +0300

    add commit

--- app/controllers/current_news_controller.rb
+++ app/controllers/current_news_controller.rb
@@ -12,7 +12,7 @@ class CurrentNewsController < ApplicationController
 
     @current_news = get_current_news()
 
-    to pass in for view stuff for the user to know which date and which category he/she select
+    # to pass in for view stuff for the user to know which date and which category he/she select
     @selected_published = fill_in_published()
     @selected_category = fill_in_category_name()
   end
