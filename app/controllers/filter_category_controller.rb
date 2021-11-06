a29596fca768111eb8d357a00583790c4080c782
loubana alarabi <loubana@dconfig.com>
2021-11-06 22:09:57 +0300

    rm categories

--- app/controllers/filter_category_controller.rb
+++ app/controllers/filter_category_controller.rb
@@ -1,9 +1,9 @@
 class FilterCategoryController < ApplicationController
   def index
-    categories = Category.find_each
     category = Category.find(params[:category_id])
     category_filter = category.current_news.group_by_day(:published).count
     chart_data = category_filter.to_a
     render json: chart_data, status: :ok
   end
+  
 end
